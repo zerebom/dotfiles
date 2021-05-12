f4f62fa55339899913fe1881b7d0ee87c9132d9d
zerebom <ice.choco.pudding.kokoro@gmail.com>
2021-05-12 09:30:40 +0900

    Divide zshrc to some files.

---
+++ .zsh/exports.zsh
@@ -0,0 +1,13 @@
+export PATH=$HOME/.wantedly/bin:$PATH
+export PATH=$GOPATH/bin:$PATH
+export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="/usr/local/bin/nvim:$PATH"
+#export PATH=$HOME/.poetry/bin:$PATH
+#export PATH=$HOME/.pyenv/bin:$PATH
+#export PATH=$HOME/.local/bin:$PATH
+#export PATH=/usr/local/bin:$PATH
+#export GOPATH=$HOME/go
+
+export GOPRIVATE=github.com/wantedly
+export EDITOR=vim
+export REFLECTION_SERVER=apis-reflection-server.apis-reflection-server:80
